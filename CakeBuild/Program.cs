75db3fd74edc15ec0f8c3b7c2ed329a0740e3be7
sekelsta <sekelsta@protonmail.com>
2025-05-16 05:18:31 -0500

    Move assets and modinfo into root directory

--- CakeBuild/Program.cs
+++ CakeBuild/Program.cs
@@ -35,7 +35,7 @@ public class BuildContext : FrostingContext
     {
         BuildConfiguration = context.Argument("configuration", "Release");
         SkipJsonValidation = context.Argument("skipJsonValidation", false);
-        var modInfo = context.DeserializeJsonFromFile<ModInfo>($"../{BuildContext.ProjectName}/modinfo.json");
+        var modInfo = context.DeserializeJsonFromFile<ModInfo>($"../modinfo.json");
         Version = modInfo.Version;
         Name = modInfo.ModID;
     }
@@ -50,7 +50,7 @@ public sealed class ValidateJsonTask : FrostingTask<BuildContext>
         {
             return;
         }
-        var jsonFiles = context.GetFiles($"../{BuildContext.ProjectName}/assets/**/*.json");
+        var jsonFiles = context.GetFiles($"../assets/**/*.json");
         foreach (var file in jsonFiles)
         {
             try
@@ -97,11 +97,11 @@ public sealed class PackageTask : FrostingTask<BuildContext>
         context.CleanDirectory("../Releases");
         context.EnsureDirectoryExists($"../Releases/{context.Name}");
         context.CopyFiles($"../{BuildContext.ProjectName}/bin/{context.BuildConfiguration}/publish/*", $"../Releases/{context.Name}");
-        context.CopyDirectory($"../{BuildContext.ProjectName}/assets", $"../Releases/{context.Name}/assets");
-        context.CopyFile($"../{BuildContext.ProjectName}/modinfo.json", $"../Releases/{context.Name}/modinfo.json");
-        if (context.FileExists($"../{BuildContext.ProjectName}/modicon.png"))
+        context.CopyDirectory($"../assets", $"../Releases/{context.Name}/assets");
+        context.CopyFile($"../modinfo.json", $"../Releases/{context.Name}/modinfo.json");
+        if (context.FileExists($"../modicon.png"))
         {
-            context.CopyFile($"../{BuildContext.ProjectName}/modicon.png", $"../Releases/{context.Name}/modicon.png");
+            context.CopyFile($"../modicon.png", $"../Releases/{context.Name}/modicon.png");
         }
         context.Zip($"../Releases/{context.Name}", $"../Releases/{context.Name}_{context.Version}.zip");
     }
