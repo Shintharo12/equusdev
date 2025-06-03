0cbb9ad28204ec76aec60841d7f1a8d4acb385aa
safwyls <safwyl@pm.me>
2025-06-03 10:06:13 -0700

    Stamina and Extra Gaits (#1)
    * Add stamina behavior and gaits
    
    * Remove bola patch
    
    * Require assets folder to exist, since we have assets
    
    * Clean up comments
    
    * Gait mapping groundwork
    
    Change curb bit to decelerate with S key
    
    Fix control scheme not loading properly
    
    Fix bug where gait can be changed while not moving
    
    Add gait indicator icon for hud
    
    adjusted animation speed and movement speed for walk and trot, removed bola patch.
    
    Fix jump animation issues
    
    Override mount events to fix dismount bug
    
    Rework gait change logic, add idle and walkback gaits
    
    Add walkback/idle states to hud watcher
    
    tweaked animation/movement speed for walk and walkback
    
    Add jump/buck anim and reorder alphabetically
    
    * Fix failed merge
    
    * Fallback to config value rather than hardcoded ease out
    
    * Prevent crash on other mountables, i.e. beds
    
    * Disable filewatcher for now as it isn't thread safe
    
    * update version
    
    * Fix configlib and duplicate animation code
    
    * Adjusted gallop animation
    
    * More gallop adjustments
    
    * updated canter animation
    
    * Use or remove unused config values, default DebugMode to false
    
    * Fix configlib setup and remove unused values
    
    * Remove now-unused lang keys, reword spawn config
    
    * Update lang calls for debugging items
    
    * Adjust stamina value visibility
    
    * Fix lang file missing comma
    
    * Default to show stamina bar always (when mounted)
    
    ---------
    
    Co-authored-by: sekelsta <sekelsta@protonmail.com>
    Co-authored-by: Shintharo12 <shania.peeters@outlook.com>

---
+++ Equus/Config/EquusConfig.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System.Reflection;
+using System;
+using Vintagestory.API.Common;
+using Vintagestory.API.MathTools;
+
+namespace Equus.Config
+{
+    public class EquusConfig
+    {
+        // Stamina
+        public bool EnableStamina { get; set; } = true;
+        public float GlobalMaxStaminaMultiplier { get; set; } = 1f;
+        public float GlobalStaminaRegenMultiplier { get; set; } = 1f;
+        
+        // Stamina Costs
+        public float GlobalSwimStaminaCostMultiplier { get; set; } = 1f;
+        public float GlobalSprintStaminaCostMultiplier { get; set; } = 1f;
+
+        // Hud
+        public bool HideStaminaOnFull { get; set; } = false;
+        public string StaminaBarLocation { get; set; } = "AboveHealth";
+        public float StaminaBarWidthMultiplier { get; set; } = 1f;
+        public float StaminaBarXOffset { get; set; } = 0f;
+        public float StaminaBarYOffset { get; set; } = 0f;
+        public bool ShowHudIcon { get; set; } = true;
+        public float IconOffsetX { get; set; } = -400f;
+        public float IconOffsetY { get; set; } = -99f;
+        public float IconSize { get; set; } = 42f;
+
+        // Debugging
+        public bool DebugMode { get; set; } = false;
+
+    }
+}
--- Equus/EquusModSystem.cs
+++ Equus/EquusModSystem.cs
@@ -1,27 +1,152 @@
-﻿using Genelib;
+﻿using Equus.Behaviors;
+using Equus.Config;
+using Equus.Hud;
+using Equus.Systems;
+using Genelib;
+using System;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
 using Vintagestory.API.Config;
 using Vintagestory.API.Server;
+using Vintagestory.Client.NoObf;
 
 namespace Equus
 {
     public class EquusModSystem : ModSystem
     {
+        private HudElementStaminaBar _staminaHud;
+        private FileWatcher _fileWatcher;
+        private long customHudListenerId;
+
+        private HudIconRenderer hudIconRenderer;
+
+        public string ModId => Mod.Info.ModID;
+        public ILogger Logger => Mod.Logger;
+        public ICoreAPI Api { get; private set; }
+        public ICoreClientAPI ClientApi { get; private set; }
+        public EquusConfig Config { get; private set; }
+        public static EquusModSystem Instance { get; private set; }
+
         // Called on server and client
         public override void Start(ICoreAPI api)
         {
+            Instance = this;
+            Api = api;
             GenomeType.RegisterInterpreter(new EquusInterpreter());
+
+            api.RegisterEntityBehaviorClass(ModId + ":rideable", typeof(EntityBehaviorEquusRideable));
+            api.RegisterEntityBehaviorClass(ModId + ":stamina", typeof(EntityBehaviorStamina));
+
+            ReloadConfig(api, false);
         }
 
         public override void StartServerSide(ICoreServerAPI api)
         {
-
+            api.Event.OnEntityLoaded += AddFatigueHandlers;
         }
 
         public override void StartClientSide(ICoreClientAPI api)
         {
+            ClientApi = api;
 
+            if (Config.EnableStamina)
+            {
+                customHudListenerId = api.Event.RegisterGameTickListener(CheckAndInitializeCustomHud, 20);
+            }
+
+            if (Config.ShowHudIcon)
+            {
+                hudIconRenderer = new HudIconRenderer(api);
+                hudIconRenderer.Initialize();
+            }
+        }
+
+        private void CheckAndInitializeCustomHud(float dt)
+        {
+            var vanillaHudStatbar = GetVanillaStatbarHud();
+
+            if (vanillaHudStatbar != null && vanillaHudStatbar.IsOpened())
+            {
+                _staminaHud = new HudElementStaminaBar(ClientApi);
+                ClientApi.Event.RegisterGameTickListener(_staminaHud.OnGameTick, 1000);
+                ClientApi.Gui.RegisterDialog(_staminaHud);
+
+                ClientApi.Event.UnregisterGameTickListener(customHudListenerId);
+            }
+        }
+
+        private HudStatbar GetVanillaStatbarHud()
+        {
+            foreach (var hud in ClientApi.Gui.OpenedGuis)
+            {
+                if (hud is HudStatbar statbar)
+                {
+                    return statbar;
+                }
+            }
+
+            return null;
+        }
+
+        private void AddFatigueHandlers(Entity entity)
+        {
+            var ebs = entity.GetBehavior<EntityBehaviorStamina>();
+            if (ebs == null) return;
+            ebs.OnFatigued += (ftg, ftgSource) => HandleFatigued(entity as EntityAgent, ftg, ftgSource);
+        }
+
+        public float HandleFatigued(EntityAgent eagent, float fatigue, FatigueSource ftgSource)
+        {
+            fatigue = ApplyFatigueProtection(eagent, fatigue, ftgSource);
+
+            return fatigue;
+        }
+
+        public float ApplyFatigueProtection(EntityAgent eagent, float fatigue, FatigueSource ftgSource)
+        {
+            return fatigue;
+        }
+
+        public void ReloadConfig(ICoreAPI api, bool isReload)
+        {
+            //(_fileWatcher ??= new FileWatcher()).Queued = true;
+
+            try
+            {
+                // Load user config
+                var _config = api.LoadModConfig<EquusConfig>($"{ModId}.json");
+
+                // If no user config, create one
+                if (_config == null)
+                {
+                    Mod.Logger.Warning("Missing config! Using default.");
+                    Config = new EquusConfig();
+                }
+                else
+                {
+                    Config = _config;
+                }
+
+                if (isReload)
+                {
+                    // Update stats
+                    // ToDo: Figure out how to update stats on reload
+                }
+                else
+                {
+                    // Only do this if we are not actively reloading
+                    // Store config again (to ensure any new props are saved)
+                    api.StoreModConfig(Config, $"{ModId}.json");
+                }
+            }
+            catch (Exception ex)
+            {
+                Mod.Logger.Error($"Could not load {ModId} config!");
+                Mod.Logger.Error(ex);
+            }
+
+           // api.Event.RegisterCallback(_ => _fileWatcher.Queued = false, 100);
         }
     }
 }
